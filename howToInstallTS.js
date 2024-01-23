d5a2149422690462f899c614687f17cd5e805c0c
Ornella B <borges.ornella@gmail.com>
2024-01-23 16:43:36 +0100

    init TS project

---
+++ howToInstallTS.js
@@ -0,0 +1,11 @@
+/* tapper la commande dans le terminal : */
+
+// initialiser package.json :  npm init -y
+// installer Ts :  npm install --save-dev typescript
+// creation du fichier de config TS : npx tsc --init
+
+
+// creaction d'un dossier dist et mettre le fichier app.js dedans
+
+// creer la compilation de TS vers JS: 
+
